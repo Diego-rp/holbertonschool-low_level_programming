bc117c189f7902390617f5b18e003d8242114e28
diego-rp <ing.diegorodriguez@gmail.com>
2020-02-05 10:19:29 -0500

    frist upload

---
+++ 0x00-hello_world/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+touch 0-preprocessor
+touch 1-compiler
+touch 2-assembler
+touch 3-name
+touch 4-puts.c
+touch 5-printf.c
+touch 6-size.c
+touch 100-intel
+touch 101-quote.c
+echo "#!/bin/bash" >> 0-preprocessor
+echo "#!/bin/bash" >> 1-compiler
+echo "#!/bin/bash" >> 2-assembler
+echo "#!/bin/bash" >> 3-name
+echo "#!/bin/bash" >> 4-puts.c
+echo "#!/bin/bash" >> 5-printf.c
+echo "#!/bin/bash" >> 6-size.c
+echo "#!/bin/bash" >> 100-intel
+echo "#!/bin/bash" >> 101-quote.c
+chmod 755 0-preprocessor
+chmod 755 1-compiler
+chmod 755 2-assembler
+chmod 755 3-name
+chmod 755 4-puts.c
+chmod 755 5-printf.c
+chmod 755 6-size.c
+chmod 755 100-intel
+chmod 755 101-quote.c
