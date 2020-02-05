982f35b95488278da9cfcdd087698df8e97dcfee
diego-rp <ing.diegorodriguez@gmail.com>
2020-02-05 16:35:47 -0500

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-  if (write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) != 59)
-    {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return -1;
-    }
- 
-    return 0;
+if (write(1, "and that piece of art is useful\" - D
+ora Korpar, 2015-10-19\n", 59) != 59)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (-1);
+}
+return (0);
 }
