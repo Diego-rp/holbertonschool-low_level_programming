fe2047721cc8387931aef822f5acdf6da60ddfa4
diego-rp <ing.diegorodriguez@gmail.com>
2020-02-06 17:57:42 -0500

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+	int n = '1';
+	int o = '2';
+
+	for (m = '0' ; m <= '9' ; m++)
+		for (n = m + 1 ; n <= '9' ; n++)
+			for (o = n + 1 ; o <= '9' ; o++)
+				{
+				putchar(m);
+				putchar(n);
+				putchar(o);
+				if ((m == '7') && (n == '8') && (o == '9'))
+					continue;
+				putchar(',');
+				putchar(' ');
+				}
+	putchar('\n');
+	return (0);
+}
